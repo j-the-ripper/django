6537467e332e4795be2d314a9a62feffa1431da9
Jay Patel <jbp@somaiya.edu>
2024-10-05 14:56:59 +0530

    Fixes #35795 -- Added role="button" to delete link for screen readers.

--- tests/admin_views/tests.py
+++ tests/admin_views/tests.py
@@ -8233,7 +8233,8 @@ class AdminKeepChangeListFiltersTests(TestCase):
 
         # Check the delete link.
         delete_link = re.search(
-            '<a href="(.*?)" class="deletelink">Delete</a>', response.content.decode()
+            '<a href="(.*?)" class="deletelink" role="button">Delete</a>',
+            response.content.decode(),
         )
         self.assertURLEqual(delete_link[1], self.get_delete_url())
 
