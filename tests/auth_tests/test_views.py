7896a4c38642dcadfde648b228291c4f871eec7a
Jay Patel <jbp@somaiya.edu>
2024-10-15 21:03:57 +0530

    change request

--- tests/auth_tests/test_views.py
+++ tests/auth_tests/test_views.py
@@ -1520,7 +1520,7 @@ class ChangelistTests(MessagesTestMixin, AuthViewsTestCase):
         response = self.client.get(user_change_url)
         # Test the link inside password field help_text.
         rel_link = re.search(
-            r'<a class="button" href="([^"]*)">Reset password</a>',
+            r'<a class="button" role="button" href="([^"]*)">Reset password</a>',
             response.content.decode(),
         )[1]
         self.assertEqual(urljoin(user_change_url, rel_link), password_change_url)
@@ -1616,7 +1616,7 @@ class ChangelistTests(MessagesTestMixin, AuthViewsTestCase):
         response = self.client.get(user_change_url)
         # Test the link inside password field help_text.
         rel_link = re.search(
-            r'<a class="button" href="([^"]*)">Set password</a>',
+            r'<a class="button" role="button" href="([^"]*)">Set password</a>',
             response.content.decode(),
         )[1]
         self.assertEqual(urljoin(user_change_url, rel_link), password_change_url)
